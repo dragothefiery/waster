a181e93ea3cf3929829e9e3947504297bf43ac3b
dragothefiery <gilbert_90@mail.ru>
2015-08-28 09:06:49 +0000

    pm2 tweaks

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 var Sequelize = require('sequelize');
-require('dotenv').load();
 
 var sequelize = new Sequelize('postgres://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + ':5432/postgres');
 
