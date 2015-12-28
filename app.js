2aef9e1c724cf7558ac057ce50be84fa2201e1af
Stas Karpov <gilbert_90@mail.ru>
2015-12-28 09:56:41 +0300

    ifttt

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var webhook = require('express-ifttt-webhook');
+
+var WorkTime = require('./work_time');
 
 require('dotenv').load();
 
@@ -15,6 +18,16 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+var methods = {
+	enter_exit_work: function(data, done) {
+		WorkTime.write(data.username);
+		done();
+	}
+};
+
+app.use(webhook(methods));
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
