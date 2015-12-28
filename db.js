b3c6a24ae624037b372c41ef915dd04121f8c837
Stas Karpov <gilbert_90@mail.ru>
2015-12-28 08:50:55 +0300

    db_host param

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('postgres://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + ':5432/postgres');
+var sequelize = new Sequelize('postgres://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + ':5432/' + process.env.DB_NAME);
 
 module.exports = sequelize;
